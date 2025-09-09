59ccc8bf83f65cc0a45f2cdcf55d26b254ae98d0
Todd Heasley <toddheasley@me.com>
2025-09-09 14:42:43 -0400

    feat(account): develop account model

--- Feature/Sources/Account/Account.swift
+++ Feature/Sources/Account/Account.swift
@@ -3,15 +3,28 @@ import Foundation
 public struct Account: Codable, Equatable, Identifiable {
     public let name: String
     public let deletePolicy: DeletePolicy
+    public let identities: [Identity]
+    public let servers: [Server]
+
+    public var incomingServer: Server? { server(.jmap) ?? server(.imap) ?? nil }
+    public var outgoingServer: Server? { server(.jmap) ?? server(.smtp) ?? nil }
 
     public init(
         name: String,
         deletePolicy: DeletePolicy = .never,
+        identities: [Identity] = [],
+        servers: [Server] = [],
         id: UUID = UUID()
     ) {
         self.name = name
         self.deletePolicy = deletePolicy
+        self.identities = identities
+        self.servers = servers
         self.id = id
+    }
+
+    func server(_ serverProtocol: ServerProtocol) -> Server? {
+        servers.filter { $0.serverProtocol == serverProtocol }.first
     }
 
     // MARK: Equatable
--- Feature/Tests/AccountTests/AccountTests.swift
+++ Feature/Tests/AccountTests/AccountTests.swift
@@ -3,5 +3,55 @@ import Testing
 import Foundation
 
 struct AccountTests {
+    @Test func incomingServer() {
+        #expect(Account(name: "user@example.com", servers: [.imap, .smtp]).incomingServer?.serverProtocol == .imap)
+        #expect(Account(name: "user@example.com", servers: [.jmap]).incomingServer?.serverProtocol == .jmap)
+        #expect(Account(name: "user@example.com", servers: [.smtp]).incomingServer == nil)
+    }
 
+    @Test func outgoingServer() {
+        #expect(Account(name: "user@example.com", servers: [.imap, .smtp]).outgoingServer?.serverProtocol == .smtp)
+        #expect(Account(name: "user@example.com", servers: [.jmap]).outgoingServer?.serverProtocol == .jmap)
+        #expect(Account(name: "user@example.com", servers: [.imap]).outgoingServer == nil)
+    }
+    
+    @Test func server() {
+        #expect(Account(name: "user@example.com", servers: [.imap, .smtp]).server(.imap)?.serverProtocol == .imap)
+        #expect(Account(name: "user@example.com", servers: [.imap, .smtp]).server(.smtp)?.serverProtocol == .smtp)
+        #expect(Account(name: "user@example.com", servers: [.imap, .smtp]).server(.jmap) == nil)
+        #expect(Account(name: "user@example.com", servers: [.jmap]).server(.jmap)?.serverProtocol == .jmap)
+        #expect(Account(name: "user@example.com", servers: [.jmap]).server(.imap) == nil)
+    }
+}
+
+private extension Server {
+    static var jmap: Self {
+        Self(
+            serverProtocol: .jmap,
+            connectionSecurity: .none,
+            authenticationType: .none,
+            username: "user",
+            hostname: "jmap.example.com"
+        )
+    }
+
+    static var imap: Self {
+        Self(
+            serverProtocol: .imap,
+            connectionSecurity: .none,
+            authenticationType: .none,
+            username: "user",
+            hostname: "imap.example.com"
+        )
+    }
+
+    static var smtp: Self {
+        Self(
+            serverProtocol: .smtp,
+            connectionSecurity: .none,
+            authenticationType: .none,
+            username: "user",
+            hostname: "smtp.example.com"
+        )
+    }
 }
