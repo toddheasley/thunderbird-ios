690ed3b10a0fe7de149ea8b91e1ba601bb2f1a10
Ashley <ashley.soucar@gmail.com>
2025-11-26 14:55:58 -0500

    Initial full alert system with feature not implemented

--- Thunderbird/Thunderbird/App.swift
+++ Thunderbird/Thunderbird/App.swift
@@ -4,12 +4,20 @@ import SwiftUI
 @main
 struct App: SwiftUI.App {
     @State private var accounts: Accounts = Accounts()
+    @State private var showAlert = false
 
     // MARK: App
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(accounts)
+            ZStack {
+                ContentView()
+                    .environment(accounts)
+                if showAlert {
+                    FeatureNotImplementedView()
+                }
+            }
+        }.onChange(of: AlertManager.shared.showAlert) {
+            showAlert = AlertManager.shared.showAlert
         }
         #if os(macOS)
         .defaultSize(width: 768.0, height: 512.0)
