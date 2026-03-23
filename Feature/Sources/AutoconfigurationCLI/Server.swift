8037e61a267b5ea55235eed11264e81157bd2e40
Todd Heasley <toddheasley@me.com>
2026-03-23 10:18:59 -0400

    feat(core): remove empty core libraries package

--- Feature/Sources/AutoconfigurationCLI/Server.swift
+++ Feature/Sources/AutoconfigurationCLI/Server.swift
@@ -1,6 +1,6 @@
 import Autoconfiguration
 
-extension [Server.Authentication] {
+extension [EmailProvider.Server.Authentication] {
     func joined(separator: String = "") -> String {
         map { $0.description }.joined(separator: separator)
     }
