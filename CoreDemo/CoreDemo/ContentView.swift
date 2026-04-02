d290dad5991987493f8ee40625fca6b43a2e896d
Todd Heasley <toddheasley@me.com>
2026-04-01 22:39:14 -0400

    feat(core): add demo project

---
+++ CoreDemo/CoreDemo/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct ContentView: View {
+
+    // MARK: View
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
