f96a05593705790af38dc22079e10a5c149fa62b
Ashley <ashley.soucar@gmail.com>
2026-04-17 10:10:41 -0400

    Settings Navigation Adds settings button nav to general settings view Closes #265

---
+++ Thunderbird/Thunderbird/Settings/GeneralSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralSettingsView.swift
+//  Thunderbird
+//
+//  Created by Ashley Soucar on 4/17/26.
+//
+import SwiftUI
+
+struct GeneralSettingsView: View {
+    @Environment(\.openURL) private var openURL
+    var body: some View {
+        NavigationStack {
+            List {
+
+            }
+        }.navigationTitle("settings_header")
+    }
+}
