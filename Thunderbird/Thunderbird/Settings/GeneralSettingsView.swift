54bff60105cb740c76df606d26a2e15d3c28a734
Ashley <ashley.soucar@gmail.com>
2026-04-27 15:54:50 -0400

    Add simple account management to settings

--- Thunderbird/Thunderbird/Settings/GeneralSettingsView.swift
+++ Thunderbird/Thunderbird/Settings/GeneralSettingsView.swift
@@ -5,19 +5,53 @@
 //  Created by Ashley Soucar on 4/17/26.
 //
 import SwiftUI
+import Account
 
 struct GeneralSettingsView: View {
     @Environment(\.openURL) private var openURL
+    @Environment(Accounts.self) private var accounts: Accounts
+
     var body: some View {
         NavigationStack {
             List {
-                NavigationLink("Feature Flags", destination: FeatureFlagDebugView())
-                Button(
-                    "donation_support",
-                    action: {
-                        guard let url = URL(string: "https://www.thunderbird.net/en-US/donate/") else { return }
-                        openURL(url)
-                    })
+                Section("General Settings") {
+
+                }
+                Section("Accounts") {
+                    ForEach(accounts.allAccounts) { account in
+                        HStack {
+                            Text(account.name)
+                            Spacer()
+                            Menu {
+                                Button(action: {
+                                    accounts.delete(account)
+                                }) {
+                                    Text("account_sign_out_button")
+                                }
+                            } label: {
+                                Label("options_button", systemImage: "ellipsis")
+                                    .labelStyle(.iconOnly)
+                                    .foregroundStyle(.black)
+                            }.compositingGroup()
+
+                        }
+                    }
+                    NavigationLink(destination: {
+                        ManualAccount()
+                    }) {
+                        Label("Add Account", systemImage: "plus")
+                    }.navigationLinkIndicatorVisibility(.hidden)
+                }
+                Section("Miscellaneous") {
+                    NavigationLink("Feature Flags", destination: FeatureFlagDebugView())
+                    Button(
+                        "donation_support",
+                        action: {
+                            guard let url = URL(string: "https://www.thunderbird.net/en-US/donate/") else { return }
+                            openURL(url)
+                        })
+                }
+
             }
         }.navigationTitle("settings_header")
     }
