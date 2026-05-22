6a40efe1df425b48db51741ce271ff26e7540b2b
Todd Heasley <toddheasley@me.com>
2026-05-22 12:38:22 -0400

    feat(account): add mailbox rights, expand roles

--- Core/Sources/Account/Mailbox.swift
+++ Core/Sources/Account/Mailbox.swift
@@ -2,8 +2,10 @@ import Foundation
 
 public struct Mailbox: CustomStringConvertible, Hashable, Identifiable {
     public typealias Role = JMAP.Mailbox.Role
+    public typealias Rights = JMAP.Mailbox.Rights
 
     public let role: Role?
+    public let rights: Rights
     public let name: String
     public let isSubscribed: Bool
     public let unreadEmails: Int?
@@ -12,12 +14,14 @@ public struct Mailbox: CustomStringConvertible, Hashable, Identifiable {
     public init(
         _ name: String,
         role: Role? = nil,
+        rights: Rights = Rights(),
         isSubscribed: Bool = false,
         unreadEmails: Int? = nil,
         totalEmails: Int? = nil,
         id: String = UUID().uuidString(1)
     ) {
         self.role = role
+        self.rights = rights
         self.name = name
         self.isSubscribed = isSubscribed
         self.unreadEmails = unreadEmails
@@ -36,7 +40,8 @@ extension Mailbox {
     init(_ mailbox: (IMAP.Mailbox, IMAP.Mailbox.Status?)) {
         self.init(
             mailbox.0.path.name.description,
-            role: mailbox.0.path.role,
+            role: mailbox.0.path.suggestedRole,
+            rights: Rights(),
             isSubscribed: !mailbox.0.attributes.filter({ $0 == .subscribed }).isEmpty,
             unreadEmails: mailbox.1?.unseenCount,
             totalEmails: mailbox.1?.messageCount
@@ -47,6 +52,7 @@ extension Mailbox {
         self.init(
             mailbox.name,
             role: mailbox.role,
+            rights: mailbox.rights,
             isSubscribed: mailbox.isSubscribed,
             unreadEmails: mailbox.unreadEmails,
             totalEmails: mailbox.totalEmails,
@@ -56,7 +62,18 @@ extension Mailbox {
 }
 
 extension IMAP.Mailbox.Path {
-    var role: Mailbox.Role? {
-        Mailbox.Role(rawValue: lastPathComponent.lowercased().components(separatedBy: " ")[0])
+    var suggestedRole: Mailbox.Role? {
+        let rawValue: String = lastPathComponent.lowercased().components(separatedBy: .whitespaces)[0]
+        if let role: Mailbox.Role = Mailbox.Role(rawValue: rawValue) {
+            return role
+        } else if rawValue.hasPrefix("spam") || rawValue.hasPrefix("junk") || rawValue.hasPrefix("bulk") {
+            return .junk
+        } else if rawValue.hasPrefix("deleted") {
+            return .trash
+        } else if rawValue.hasPrefix("draft") {
+            return .drafts
+        } else {
+            return nil
+        }
     }
 }
--- Core/Tests/AccountTests/MailboxTests.swift
+++ Core/Tests/AccountTests/MailboxTests.swift
@@ -3,13 +3,15 @@ import Testing
 import Foundation
 
 struct MailboxPathTests {
-    @Test func role() throws {
-        #expect(try IMAP.Mailbox.Path(name: "INBOX").role == .inbox)
-        #expect(try IMAP.Mailbox.Path(name: "[Gmail]/Inbox", pathSeparator: "/").role == .inbox)
-        #expect(try IMAP.Mailbox.Path(name: "Sent Mail").role == .sent)
-        #expect(try IMAP.Mailbox.Path(name: "Deleted Items").role == nil)
-        #expect(try IMAP.Mailbox.Path(name: "junk mail").role == .junk)
-        #expect(try IMAP.Mailbox.Path(name: "Spam").role == nil)
-        #expect(try IMAP.Mailbox.Path(name: "Trash").role == .trash)
+    @Test func suggestedRole() throws {
+        #expect(try IMAP.Mailbox.Path(name: "INBOX").suggestedRole == .inbox)
+        #expect(try IMAP.Mailbox.Path(name: "[Gmail]/Inbox", pathSeparator: "/").suggestedRole == .inbox)
+        #expect(try IMAP.Mailbox.Path(name: "Sent Mail").suggestedRole == .sent)
+        #expect(try IMAP.Mailbox.Path(name: "Deleted Items").suggestedRole == .trash)
+        #expect(try IMAP.Mailbox.Path(name: "junk mail").suggestedRole == .junk)
+        #expect(try IMAP.Mailbox.Path(name: "Spam").suggestedRole == .junk)
+        #expect(try IMAP.Mailbox.Path(name: "Trash").suggestedRole == .trash)
+        #expect(try IMAP.Mailbox.Path(name: "drafts").suggestedRole == .drafts)
+        #expect(try IMAP.Mailbox.Path(name: "Draft").suggestedRole == .drafts)
     }
 }
