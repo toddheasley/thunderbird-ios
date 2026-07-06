8e3cf9a4a8b390e8020db3682ec4dea86977ea90
Ashley <ashley.soucar@gmail.com>
2026-07-06 15:51:53 -0400

    wip avatar

---
+++ Thunderbird/Thunderbird/Util/AvatarView.swift
@@ -0,0 +1,48 @@
+// AvatarView.swift
+// Thunderbird
+//
+// Created by Ashley Soucar on 6/30/26.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+//
+
+import SwiftUI
+import EmailAddress
+
+struct AvatarView: View {
+    private var avatarText: String = ""
+    private var bubbleColor: Color = .clear
+
+    init(displayName: String = "", emailAddress: EmailAddress, bubbleColor: Color) {
+        if !displayName.isEmpty {
+            avatarText = createAvatarText(displayName: displayName)
+        } else {
+            avatarText = String(emailAddress.value.first!)
+        }
+        self.bubbleColor = bubbleColor
+    }
+
+    var body: some View {
+        Text(avatarText)
+            .font(.body)
+            .overlay {
+                Circle()
+                    .stroke(
+                        bubbleColor,
+                        style: StrokeStyle(
+                            lineWidth: 1
+                        )
+                    )
+                    .fill(bubbleColor.opacity(0.15))
+                    .padding(3)
+                    .frame(width: 40, height: 40)
+
+            }
+    }
+}
+
+#Preview {
+    AvatarView(displayName: "blakls ddfas", emailAddress: EmailAddress("blah@blah.com"), bubbleColor: .yellow)
+}
---
+++ Thunderbird/Thunderbird/Util/UtilFunctions.swift
@@ -0,0 +1,21 @@
+// UtilFunctions.swift
+// Thunderbird
+//
+// Created by Ashley Soucar on 7/6/26.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+//
+
+import Foundation
+
+func createAvatarText(displayName: String) -> String {
+    let splitName = displayName.split(separator: " ")
+    if splitName.count >= 2 {
+        let letter = "\(splitName[0].capitalized.first!)\(splitName[1].capitalized.first!)"
+        return letter
+    } else {
+        return String(splitName[0].capitalized.first!)
+    }
+}
