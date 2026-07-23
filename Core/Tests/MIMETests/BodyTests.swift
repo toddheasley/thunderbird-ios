98ea0da90c723463d01119be963c414836d276c2
Todd Heasley <toddheasley@me.com>
2026-07-23 14:37:29 -0400

    feat(mime): add MIME body mock tests

--- Core/Tests/MIMETests/BodyTests.swift
+++ Core/Tests/MIMETests/BodyTests.swift
@@ -7,71 +7,84 @@ import Foundation
 import Testing
 
 struct BodyTests {
-    @Test func headers() throws {
-        #expect(
-            try Body(.aol).headers == [
-                try! Header(.contentType, "multipart/alternative; boundary=\"-=Part.d96da9.c8ced2f941b41333.19beaf8523d.267666a9a62272b=-\""),
-                .mimeVersion
-            ])
-        #expect(
-            try Body(.fastmail).headers == [
-                try! Header(.contentTransferEncoding, "8bit"),
-                try! Header(.contentType, "multipart/alternative; boundary=\"_----------=_17617196041979919223967\""),
-                .mimeVersion
-            ])
-        #expect(
-            try Body(.icloud).headers == [
-                try! Header(.contentType, "multipart/alternative; boundary=\"----=_Part_15950895_843396275.1764942606546\""),
-                .mimeVersion
-            ])
-        #expect(
-            try Body(.outlook).headers == [
-                try! Header(.contentType, "multipart/alternative; boundary=\"b1_dd99cd789bcc10ebb82bcc39304a9664\""),
-                .mimeVersion
-            ])
-        #expect(
-            try Body(.posteo).headers == [
-                try! Header(.contentType, "multipart/alternative; boundary=\"MULTIPART-MIXED-BOUNDARY\""),
-                .mimeVersion
-            ])
+    @Test(arguments: mocks.map { $0.uuid }) func headers(_ uuid: String) throws {
+        let data: Data = try .mock(uuid)
+        let body: Body = try Body(data)
+
+        switch uuid {
+        case "2EA571DD":
+            #expect(
+                body.headers == [
+                    try! Header(.contentType, "multipart/alternative; boundary=\"-=Part.d96da9.c8ced2f941b41333.19beaf8523d.267666a9a62272b=-\""),
+                    .mimeVersion
+                ])
+        case "1759430F":
+            #expect(
+                body.headers == [
+                    try! Header(.contentTransferEncoding, "8bit"),
+                    try! Header(.contentType, "multipart/alternative; boundary=\"_----------=_17617196041979919223967\""),
+                    .mimeVersion
+                ])
+        case "89526045":
+            #expect(
+                body.headers == [
+                    try! Header(.contentType, "multipart/alternative; boundary=\"----=_Part_15950895_843396275.1764942606546\""),
+                    .mimeVersion
+                ])
+        case "BFA06D93":
+            #expect(
+                body.headers == [
+                    try! Header(.contentType, "multipart/alternative; boundary=\"b1_dd99cd789bcc10ebb82bcc39304a9664\""),
+                    .mimeVersion
+                ])
+        case "F02140B7":
+            #expect(
+                body.headers == [
+                    try! Header(.contentType, "multipart/alternative; boundary=\"MULTIPART-MIXED-BOUNDARY\""),
+                    .mimeVersion
+                ])
+        default:
+            break
+        }
     }
 
-    @Test func descriptionInit() throws {
-        let aol: Body = try Body(.aol)
-        #expect(aol.contentType == .multipart(.alternative, try! Boundary("-=Part.d96da9.c8ced2f941b41333.19beaf8523d.267666a9a62272b=-")))
-        #expect(aol.contentTransferEncoding == nil)
-        #expect(try aol.part.parts.first?.contentType == .text(.html, .utf8))
-        #expect(try aol.part.parts.count == 1)
-        let fastmail: Body = try Body(.fastmail)
-        #expect(fastmail.contentType == .multipart(.alternative, try! Boundary("_----------=_17617196041979919223967")))
-        #expect(fastmail.contentTransferEncoding == .data)
-        #expect(try fastmail.part.parts.first?.contentType == .text(.plain, .utf8))
-        #expect(try fastmail.part.parts.count == 2)
-        let icloud: Body = try Body(.icloud)
-        #expect(icloud.contentType == .multipart(.alternative, try! Boundary("----=_Part_15950895_843396275.1764942606546")))
-        #expect(icloud.contentTransferEncoding == nil)
-        #expect(try icloud.part.parts.first?.contentType == .text(.html, .iso8859))
-        #expect(try icloud.part.parts.count == 1)
-        let outlook: Body = try Body(.outlook)
-        #expect(outlook.contentType == .multipart(.alternative, try! Boundary("b1_dd99cd789bcc10ebb82bcc39304a9664")))
-        #expect(outlook.contentTransferEncoding == nil)
-        #expect(try outlook.part.parts.first?.contentType == .text(.plain, .utf8))
-        #expect(try outlook.part.parts.count == 2)
-        let posteo: Body = try Body(.posteo)
-        #expect(posteo.contentType == .multipart(.alternative, try! Boundary("MULTIPART-MIXED-BOUNDARY")))
-        #expect(posteo.contentTransferEncoding == nil)
-        #expect(try posteo.part.parts.first?.contentType == .text(.html, .utf8))
-        #expect(try posteo.part.parts.count == 2)
+    @Test(arguments: mocks.map { $0.uuid }) func descriptionInit(_ uuid: String) throws {
+        let body: Body = try Body(.mock(uuid))
+        switch uuid {
+        case "2EA571DD":
+            #expect(body.contentType == .multipart(.alternative, try! Boundary("-=Part.d96da9.c8ced2f941b41333.19beaf8523d.267666a9a62272b=-")))
+            #expect(body.contentTransferEncoding == nil)
+            #expect(try body.part.parts.first?.contentType == .text(.html, .utf8))
+            #expect(try body.part.parts.count == 1)
+        case "1759430F":
+            #expect(body.contentType == .multipart(.alternative, try! Boundary("_----------=_17617196041979919223967")))
+            #expect(body.contentTransferEncoding == .data)
+            #expect(try body.part.parts.first?.contentType == .text(.plain, .utf8))
+            #expect(try body.part.parts.count == 2)
+        case "89526045":
+            #expect(body.contentType == .multipart(.alternative, try! Boundary("----=_Part_15950895_843396275.1764942606546")))
+            #expect(body.contentTransferEncoding == nil)
+            #expect(try body.part.parts.first?.contentType == .text(.html, .iso8859))
+            #expect(try body.part.parts.count == 1)
+        case "BFA06D93":
+            #expect(body.contentType == .multipart(.alternative, try! Boundary("b1_dd99cd789bcc10ebb82bcc39304a9664")))
+            #expect(body.contentTransferEncoding == nil)
+            #expect(try body.part.parts.first?.contentType == .text(.plain, .utf8))
+            #expect(try body.part.parts.count == 2)
+        case "F02140B7":
+            #expect(body.contentType == .multipart(.alternative, try! Boundary("MULTIPART-MIXED-BOUNDARY")))
+            #expect(body.contentTransferEncoding == nil)
+            #expect(try body.part.parts.first?.contentType == .text(.html, .utf8))
+            #expect(try body.part.parts.count == 2)
+        default:
+            break
+        }
     }
 
     // MARK: RawRepresentable
-    @Test func rawValue() throws {
-        print(String(data: try Body(.fastmail).rawValue, encoding: .ascii) ?? "nil")
-        #expect(try Body(.aol).rawValue.count == 4822)
-        #expect(try Body(.fastmail).rawValue.count == 20589)
-        #expect(try Body(.icloud).rawValue.count == 8795)
-        #expect(try Body(.outlook).rawValue.count == 69543)
-        #expect(try Body(.posteo).rawValue.count == 89986)
+    @Test(arguments: mocks) func rawValue(_ mock: Mock) throws {
+        print("\(mock.uuid): \(try Body(try .mock(mock.uuid)).rawValue.count)")
+        #expect(try Body(try .mock(mock.uuid)).rawValue.count == mock.bytes)
     }
 }
 
@@ -82,20 +95,33 @@ extension BodyTests {
         #expect(Body.empty.contentType == .text(.plain, .ascii))
     }
 
-    @Test func isEmpty() throws {
-        #expect(try Body(.aol).isEmpty == false)
-        #expect(try Body(.fastmail).isEmpty == false)
-        #expect(try Body(.icloud).isEmpty == false)
-        #expect(try Body(.outlook).isEmpty == false)
-        #expect(try Body(.posteo).isEmpty == false)
+    @Test(arguments: mocks.map { $0.uuid }) func isEmpty(_ uuid: String) throws {
+        #expect(try Body(try .mock(uuid)).isEmpty == false)
         #expect(Body.empty.isEmpty == true)
     }
 }
 
 private extension Data {
-    static var aol: Self { try! Bundle.module.data(forResource: "mime-body-aol.eml") }
-    static var fastmail: Self { try! Bundle.module.data(forResource: "mime-body-fastmail.eml") }
-    static var icloud: Self { try! Bundle.module.data(forResource: "mime-body-icloud.eml") }
-    static var outlook: Self { try! Bundle.module.data(forResource: "mime-body-outlook.eml") }
-    static var posteo: Self { try! Bundle.module.data(forResource: "mime-body-posteo.eml") }
+    static func mock(_ uuid: String) throws -> Self {
+        try Bundle.module.data(forResource: "mime-body-\(uuid).eml")
+    }
 }
+
+typealias Mock = (uuid: String, bytes: Int)
+
+// Short UUID strings correspond to `mime-body`-prefixed `.eml` file in test `Resources`
+private let mocks: [Mock] = [
+    ("2EA571DD", 4822),
+    ("976C6A94", 30905),
+    ("86925F24", 7800),
+    ("7A690F43", 1733),
+    ("60EB5CAE", 6115),
+    ("F02140B7", 89986),
+    ("BFA06D93", 69543),
+    ("89526045", 8795),
+    ("E18BEE81", 21153),
+    ("2874E3C9", 106182),
+    ("1759430F", 20589),
+    ("E1FA0690", 148684),
+    ("CFD4D3A3", 591664)
+]
