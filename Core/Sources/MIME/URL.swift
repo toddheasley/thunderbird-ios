4ee7341582316317d7876df3cbeaecd06282bd75
Todd Heasley <toddheasley@me.com>
2026-08-06 14:00:21 -0400

    feat(mime): add URI to Content-ID

---
+++ Core/Sources/MIME/URL.swift
@@ -0,0 +1,33 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+
+extension URL {
+
+    /// Use ``MessageID`` with optional part ``ContentID`` as URI with `mid:` scheme
+    /// Described in [RFC 2392](https://www.rfc-editor.org/info/rfc2392)
+    public init(messageID: MessageID, contentID: ContentID? = nil) throws {
+        guard let messageID: String = messageID.description.ascii,
+            var url: Self = Self(string: "mid:\(messageID.unwrapping("<", ">"))")
+        else {
+            throw MIMEError.headerValueNotASCII
+        }
+        if let contentID: String = contentID?.description.ascii {
+            url = url.appending(path: contentID.unwrapping("<", ">"))
+        }
+        self = url
+    }
+
+    /// Use ``ContentID`` as URI with `cid:` scheme
+    /// Described in [RFC 2392](https://www.rfc-editor.org/info/rfc2392)
+    public init(contentID: ContentID) throws {
+        guard let contentID: String = contentID.description.ascii,
+            let url: Self = Self(string: "cid:\(contentID.unwrapping("<", ">"))")
+        else {
+            throw MIMEError.headerValueNotASCII
+        }
+        self = url
+    }
+}
---
+++ Core/Tests/MIMETests/URLTests.swift
@@ -0,0 +1,30 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+@testable import MIME
+import Testing
+
+struct URLTests {
+    @Test func messageIDInit() throws {
+        #expect(try URL(messageID: MessageID("<1762463150.A51D5B17@example.com>")).absoluteString == "mid:1762463150.A51D5B17@example.com")
+        #expect(
+            try URL(
+                messageID: MessageID("<1762463150.A51D5B17@example.com>"),
+                contentID: ContentID("part1.example.1762463150.A51D5B17")
+            ).absoluteString == "mid:1762463150.A51D5B17@example.com/part1.example.1762463150.A51D5B17"
+        )
+        #expect(throws: MIMEError.headerValueNotASCII) {
+            try URL(messageID: MessageID("not.æscii"))
+        }
+    }
+
+    @Test func contentIDInit() throws {
+        #expect(try URL(contentID: ContentID("<1762463150.A51D5B17@example.com>")).absoluteString == "cid:1762463150.A51D5B17@example.com")
+        #expect(try URL(contentID: ContentID("part1.example.1762463150.A51D5B17")).absoluteString == "cid:part1.example.1762463150.A51D5B17")
+        #expect(throws: MIMEError.headerValueNotASCII) {
+            try URL(contentID: ContentID("not.æscii"))
+        }
+    }
+}
