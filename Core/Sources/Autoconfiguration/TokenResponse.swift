0776978bd804d5e145b36103c8594d4e1d338c8a
Ashley <ashley.soucar@gmail.com>
2026-08-06 09:33:21 -0400

    PR suggestion

--- Core/Sources/Autoconfiguration/TokenResponse.swift
+++ Core/Sources/Autoconfiguration/TokenResponse.swift
@@ -8,8 +8,22 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 //
 
-public struct TokenResponse: Codable {
-    public let access_token: String
-    public let expires_in: Int
-    public let refresh_token: String
+public struct TokenResponse: Decodable {
+    public let accessToken: String
+    public let expiresIn: Int
+    public let refreshToken: String
+
+    // MARK: Decodable
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: Key.self)
+        accessToken = try container.decode(String.self, forKey: .accessToken)
+        expiresIn = try container.decode(Int.self, forKey: .expiresIn)
+        refreshToken = try container.decode(String.self, forKey: .refreshToken)
+    }
+
+    private enum Key: String, CodingKey {
+        case accessToken = "access_token"
+        case expiresIn = "expires_in"
+        case refreshToken = "refresh_token"
+    }
 }
